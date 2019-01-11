db71e53b1c47b8cc772ec45dd7158db45a67fb36
John_Meadow <armitage3@gazeta.pl>
2019-01-11 07:22:06 +0100

    Added selection in 3d example

---
+++ godot_3/final/selection_in_3d/camera.gd
@@ -0,0 +1,40 @@
+
+extends Camera
+
+var camera_target             = Vector3(0,0,0)
+var camera_up_vector          = Vector3(0,1,0)
+
+var camera_distance_to_center = 8
+
+var yaw         = 0
+var pitch       = 0
+
+var sensitivity = 0.15
+var scoll_speed = 2
+var mouse_button_down = false
+
+func _ready():
+#	Input.set_mouse_mode( Input.MOUSE_MODE_CAPTURED )
+	pass
+	
+func _physics_process(dt):
+	var x = camera_distance_to_center * sin(deg2rad(yaw)) * cos(deg2rad(pitch))
+	var y = camera_distance_to_center * sin(deg2rad(pitch))
+	var z = camera_distance_to_center * cos(deg2rad(yaw)) * cos(deg2rad(pitch))
+
+	look_at_from_position( Vector3 ( x, y, z ), camera_target, camera_up_vector )
+
+func _input(event):
+	if (event is InputEventMouseButton):
+		mouse_button_down = false
+		if (event.pressed and event.button_index == 2):
+			mouse_button_down = true
+		if (event.pressed and event.button_index == 4):
+			camera_distance_to_center = clamp( camera_distance_to_center - scoll_speed, 4, 400)
+
+		if (event.pressed and event.button_index == 5):
+			camera_distance_to_center = clamp( camera_distance_to_center + scoll_speed, 4, 400)
+
+	if ( event is InputEventMouseMotion && mouse_button_down ):
+		yaw     -= event.relative.x * sensitivity
+		pitch    = clamp ( pitch + event.relative.y * sensitivity,-80,80)
